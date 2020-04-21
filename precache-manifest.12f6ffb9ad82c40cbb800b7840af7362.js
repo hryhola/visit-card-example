50fed51c0c5018dcb0d5d2b89bdec180080dd9c8
VladGrygola <vladgrygola@gmail.com>
2020-04-21 23:50:38 +0300

    Updates

---
+++ precache-manifest.12f6ffb9ad82c40cbb800b7840af7362.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c129d3a30949fd28cb77fe80081476c1",
+    "url": "/visit-card-example/index.html"
+  },
+  {
+    "revision": "2fe38545d955301fc60d",
+    "url": "/visit-card-example/static/css/main.56af0dd8.chunk.css"
+  },
+  {
+    "revision": "7e4fd8f1fcf12827326f",
+    "url": "/visit-card-example/static/js/2.5f672182.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/visit-card-example/static/js/2.5f672182.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fe38545d955301fc60d",
+    "url": "/visit-card-example/static/js/main.a81f47c4.chunk.js"
+  },
+  {
+    "revision": "0b2edb92ca97ce11d52d",
+    "url": "/visit-card-example/static/js/runtime-main.392bcfd0.js"
+  }
+]);
